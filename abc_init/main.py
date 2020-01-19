f63f24dabdb1027ab51c6e67ef6cab4a8cdaa82b
515hikaru <12kojima.takahiro@gmail.com>
2020-01-19 16:22:56 +0900

    仮作成

--- abc_init/main.py
+++ abc_init/main.py
@@ -1,9 +1,23 @@
 """
 entry point
 """
+import pathlib
+
 
 __version__ = "0.1.0"
 
 
 def main():
-    pass
+    current = pathlib.Path(".")
+    for char in ["A", "B", "C", "D", "E", "F"]:
+        file_path = current / f"{char}.cpp"
+        with file_path.open("w") as f:
+            f.write(
+                """#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+"""
+            )

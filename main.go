4c359eca03a319acb20bf63c7f7d7667eaff9e9b
515hikaru <12kojima.takahiro@gmail.com>
2020-02-22 07:18:44 +0900

    feature: can specify the file name

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 const version = "v0.0.2-dev"
 
 var (
-	fileNames = []string{"A", "B", "C", "D", "E", "F"}
-	contens   = `#include <iostream>
+	app        *cli.App
+	targetName string
+	fileNames  = []string{"A", "B", "C", "D", "E", "F"}
+	contents   = `#include <iostream>
 #include <cmath>
 #include <string>
 #include <algorithm>
@@ -32,14 +36,52 @@ int main() {
 `
 )
 
-func main() {
-	b := []byte(contens)
-	for _, fileName := range fileNames {
-		fileNameWExt := fmt.Sprintf("%s.cpp", fileName)
-		err := ioutil.WriteFile(fileNameWExt, b, 0666)
+func createFile(content []byte, fileName string, fileType string) error {
+	fileType = "cpp" // TODO: support other language
+	fileNameWithExt := fmt.Sprintf("%s.%s", fileName, fileType)
+	return ioutil.WriteFile(fileNameWithExt, content, 0666)
+}
+
+func run(c *cli.Context) error {
+	b := []byte(contents)
+	if fileName := c.String("output"); fileName != "" {
+		err := createFile(b, fileName, "cpp")
 		if err != nil {
 			log.Fatal(err)
 			os.Exit(1)
 		}
+		return nil
+	}
+	for _, fileName := range fileNames {
+		err := createFile(b, fileName, "cpp")
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	app = &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "output",
+				Aliases: []string{"o"},
+				Value:   "",
+				Usage:   "Place the output into `FILE`",
+			},
+		},
+		Name:   "abc-init",
+		Usage:  "Initialization Command For AtCoder Beginner Contest.",
+		Action: run,
+	}
+}
+
+func main() {
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
